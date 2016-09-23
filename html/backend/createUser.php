93101430c6a899607e8ee1d402d99eb3dd1ddbf6
Justin Riddell <arghnews@hotmail.co.uk>
2016-09-23 14:03:45 +0100

    This is the actual commit, I am stupid. Php classes working to wrap forms and all nicely

--- html/backend/createUser.php
+++ html/backend/createUser.php
@@ -5,43 +5,179 @@
 	
 	$pdo = connect();
 
+	p("Creating fieldlist");
+	$fields = new FieldList($_POST,"username","password");
+	p("Created fieldlist");
 	
-	// apparently salt is all handled for you using this snazzy bcrypt build in library!
-	$username = $_POST['username'];
-	p($username." is username received");
-	$password = sanitise($_POST["password"]);
-	p($password." is password received");
-	$hash = password_hash($password,PASSWORD_BCRYPT); # always 72 chars long
-	p($hash);
-	$comment = sanitise("Comment here!");
-	
-	$success = false;
-	$successText = "";
+	p("Fieldlist object should all data received - printing it as json below");
+	p($fields);
+
+	// set password field value to hash
+	$passObj = $fields->getField("password");
+	$hash = password_hash($passObj->getValue(),PASSWORD_BCRYPT); # always 72 chars long
+	$passObj->setValue($hash);
+
+	$comment = "Comment field, unused currently!";
 	// if username not taken create user
-	if ( !usernameTaken($username, $pdo) ) {
+	if ( !usernameTaken($fields->getValue("username"), $pdo) ) {
 		// create user in db
-		$query = "insert into users (username,hash,comment) values (?,?,?)";
-		$stmt = $pdo->prepare($query);
-		$stmt->execute([$username,$hash,$comment]);
-		p("Would redirect now to other page - obviously if you're seeing this, it won't! Account creation success");
-		$success = true;
-		$successText = "Your account has been created!";
-		//header("Location: /");
-		//die();
+		try {
+			$query = "insert into users (username,hash,comment) values (?,?,?)";
+			$stmt = $pdo->prepare($query);
+			$stmt->execute([$fields->getValue("username"),$fields->getValue("password"),$comment]);
+			p("User successfully created");
+		} catch (Exception $e) {
+			p("Database error ".$e->getMessage());
+			$fields->setText("Could not create user, database error");
+			$fields->setSuccess(false);
+		}
+		
 	} else {
-		p("Would redirect now to other page - obviously if you're seeing this, it won't! Username was taken");
-		$successText = "Username taken";
-		//header("Location: /register.php");
-		//die();
+		$fields->getField("username")->setText("username taken");
+		$fields->getField("username")->setSuccess(false);
+	}
+	echo json_encode($fields->toJson());
+
+	// wrapper for fields
+	// example below, although it's fairly negative so don't look at it for too long
+	//{
+	//	success: false
+	//	text: "Could not create user" -- this text may/will be printed to user, make friendly
+	//	"data": {
+	//				success: false, // is O(n) call in number of fields, AND of all successes
+	//				fields: {
+	//					"username" => Field obj, ...
+	//				}
+	//			}
+	//}
+	// setting the success variable manually will override other settings
+	// useful if database error then just fail
+	class FieldList {
+		private $success;
+		private $text;
+		private $fields; // array of Field objects
+		
+		public function setSuccess($bool) {
+			$this->success = $bool;
+		}
+
+		public function setText($text) {
+			$this->text = $text;
+		}
+
+		// convenient wrapper so can go $FieldListInstance->getValue("username")
+		public function getValue($name) {
+			return $this->getField($name)->getValue();
+		}
+
+		// boolean, meant to be all success values ANDed
+		public function getSuccess() {
+			// set false if error has occurred, can return false immediately
+			if ( $this->success === false ) {
+				return false;
+			}
+			foreach ($this->fields as $field) {
+				if ( $field->getSuccess() === false) {
+					$this->success = false;
+					break;
+				}
+			}
+			return $this->success;
+		}
+
+		// requires PHP >= 5.6 (which I (obviously) have)
+		// where $fields is "username","password".. html name attribs
+		// eg. new FieldList($_POST,"username","password")
+		public function __construct($array, ...$names) {
+			$this->fields = array();
+			foreach($names as $name) {
+				// yes this could print passwords in plaintext
+				// but for debugging sanity I shall take this risk
+				// should only ever see this serverside for debugging anyway
+				p("Hi mum! ".$name." ".$array[$name]);
+				$this->fields += [ $name => (new Field($array[$name])) ];
+			}
+			$this->success = true;
+		}
+
+		// $name should be string that is field name
+		// returns Field object for editting
+		public function getField($name) {
+			return $this->fields[$name];
+		}
+
+		// maybe can just use json_encode dur?
+		public function toJson() {
+			$data = array();
+			foreach ($this->fields as $name => $field) {
+				$data += [$name => $field->toJson()];
+			}
+			$top = array("success"=>$this->getSuccess(),"data"=>$data);
+			return $top;
+		}
+
+		public function __toString() {
+			return json_encode($this->toJson());
+		}
 	}
 
-	$return = array("success"=>$success,"successText"=>$successText);
-	echo json_encode($return);
+	// these are created with success as true by default
+	// corresponds to a field in a html form
+	// text var will be used in js on page to inform user of the problem
+	//{
+	//	"data": {
+	//				"success": false,
+	//				"text": "Username taken"
+	//				"value": "bill25"
+	//			}
+	//}
+
+	// this class should probably be private?
+	class Field {
+		private $data;
+		public function __construct($value) {
+			$this->data = array("success"=>true,"text"=>"","value"=>$value);
+		}
+
+		// $name should be html form input attrib name, eg. "username"
+		public function getValue() {
+			return $this->data["value"];
+		}
+
+		public function setValue($value) {
+			$this->data["value"] = $value;
+		}
+		
+		// takes boolean
+		public function setSuccess($success) {
+			$this->data["success"] = $success;
+		}
+
+		// returns the value of success property
+		// if used success() would call func "success"
+		public function getSuccess() {
+			return $this->data["success"];
+		}
+		
+		// takes string, ie. failed because of db error
+		public function setText($text) {
+			$this->data["text"] = $text;
+		}
+
+		public function toJson() {
+			return array("data"=>$this->data);
+		}
+		
+		public function __toString() {
+			return json_encode($this->toJson());
+		}
+
+	}
 	
 	// checks if the username is taken in the database, returns true if not taken
 	function usernameTaken($username, $pdo) {
 		// check if users exists already
-		$query = "select count(*) as number from users where username=?";
+		$query = "select count(*) as number from users where username=? limit 1";
 		$stmt = $pdo->prepare($query);
 		$stmt->execute([$username]);
 		$result = $stmt->fetch();
